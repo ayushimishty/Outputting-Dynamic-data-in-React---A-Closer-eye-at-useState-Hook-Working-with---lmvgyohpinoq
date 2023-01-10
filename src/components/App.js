c86cef04aaae54be75fa47c3bb9dc6d15e59f9e3
Ayushi Jain <111980295+ayushimishty@users.noreply.github.com>
2023-01-10 16:00:38 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React,{useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 const App = () => {
-//code here
+  const [data, setData] = useState("");
+  const [fdata, setFData] = useState("");
+  function showData() {
+    setFData(data);
+  }
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id="input" onChange={(e) => setData(e.target.value)}></input>
+      <button id="button" onClick={showData}>
+        Click
+      </button>
+      <p id="text">
+        {" "}
+        Hello my name is {fdata === "" ? "____" : fdata} and I study at Newton
+        School
+      </p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
